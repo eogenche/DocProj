ae80a1ac2393a767d4504e3a3c21c26fcf00c0cd
monte <eogenche@yahoo.com>
2020-04-09 23:09:05 +0300

    adding changes

---
+++ script.js
@@ -0,0 +1,22 @@
+  $(document).ready(function() {
+    $("#D").click(function() {
+      $("#c").toggle();
+      $("#c1").toggle();
+    });
+    $("#D1").click(function() {
+      $("#c1").toggle();
+      $("#c2").toggle();
+    });
+    $("#D2").click(function() {
+      $("#c2").toggle();
+      $("#c3").toggle();
+    });
+    $("#D3").click(function() {
+      $("#c3").toggle();
+      $("#c4").toggle();
+    });
+    $("#D4").click(function() {
+      $("#c4").toggle();
+      $("#c5").toggle();
+    });
+  });
